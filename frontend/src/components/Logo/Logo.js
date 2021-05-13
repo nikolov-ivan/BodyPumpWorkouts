89d9deb305f5dc13e26a4a1d5e95455536f07cde
David Asenov <david.asenoff@gmail.com>
2021-05-13 14:27:45 +0300

    Create Header, Nav and Logo components

---
+++ frontend/src/components/Logo/Logo.js
@@ -0,0 +1,8 @@
+import logo from './../../img/logo.png';
+import './../Logo/Logo.css'
+
+const Logo = () => {
+    return <div className="logo"><a href="./index.html"><img src={logo} alt="Logo"/></a></div>
+};
+
+export default Logo;
