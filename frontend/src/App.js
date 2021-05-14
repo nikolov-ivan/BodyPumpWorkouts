ab798cdeff690ed9bc2f0e0da161bdb9c2017edb
David Asenov <david.asenoff@gmail.com>
2021-05-14 10:40:20 +0300

    Add bootstrap, change background color, rearrange imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import Logo from './components/Logo/Logo';
 import Nav from './components/Nav/Nav';
-import 'react-bootstrap';
+import './App.css';
 
 function App() {
   return (
