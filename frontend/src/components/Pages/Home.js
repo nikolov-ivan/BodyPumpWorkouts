fb1503306a5d25d8793f7a1a3aa4c8ac4aad4ab5
David Asenov <david.asenoff@gmail.com>
2021-05-19 13:51:56 +0300

    Add Pricing component

--- frontend/src/components/Pages/Home.js
+++ frontend/src/components/Pages/Home.js
@@ -1,10 +1,12 @@
 import Hero from "../Hero";
 import ChoseUs from "../ChoseUs/ChoseUs";
+import Pricing from "../Pricing/Pricing";
 const Home = () => {
   return (
     <div>
       <Hero />
       <ChoseUs/>
+      <Pricing/>
     </div>
   );
 };
